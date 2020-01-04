30b78ab07921233797a6f67ad9c93cd494bf8e24
Megan <meganwei@gmail.com>
2020-01-04 23:58:34 +0200

    Edited the about, home, and store html pages and app.py

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 
 ##### Code here ######
+@app.route('/')
+def home():
+    return render_template('home.html')
 
+@app.route('/store')
+def store():
+    return render_template('store.html')
 
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 #####################
 
 
